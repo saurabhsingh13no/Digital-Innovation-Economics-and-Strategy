ccca3b72d863fbefca6816f6f088c557f5c2ec00
saurabhsingh13nov <singh.saurab@husky.neu.edu>
2017-06-18 21:03:25 -0400

    added allAbrahms

--- parseXML.py
+++ parseXML.py
@@ -2,20 +2,23 @@ import xmltodict, json
 import sys
 from bs4 import BeautifulSoup
 
+class Wos():
 
+    def __init__(self):
+        pass
 
-def convertXMLToJson(contents):
-    o = xmltodict.parse(contents)
-    return json.dumps(o)
-
+    def convertXMLToJson(self,contents):
+        o = xmltodict.parse(contents)
+        return json.dumps(o)
 
 
 def main():
     file = "./recordForAbrahms"
     infile = open(file)
     contents = infile.read()
-    json=convertXMLToJson(contents)
-    return json
+    client=Wos()
+    json=client.convertXMLToJson(contents)
+    print (json)
 
 
 
